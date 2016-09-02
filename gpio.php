2d7c481344a527a8eced19f1c85f74fc506eddc0
ehab <ehab.mohamed.nasr@gmail.com>
2016-09-03 01:56:50 +0200

    Initial commit

---
+++ gpio.php
@@ -0,0 +1,13 @@
+<form method="get" action="gpio.php">
+    <input type="submit" value="ON" name="on">
+    <input type="submit" value="OFF" name="off">
+</form>
+<?php
+exec("gpio mode 15 out");
+if(isset($_GET['on'])){
+    exec("gpio write 15 1");
+    echo "LED is on";}
+else if(isset($_GET['off'])){
+    exec("gpio -g write 17 0");
+    echo "LED is off";}
+?>
