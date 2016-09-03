5baeaab4f337f4b2780bd7b84b6bf9a72c1443a7
ehab <ehab.mohamed.nasr@gmail.com>
2016-09-03 02:09:51 +0200

    still learning

--- gpio.php
+++ gpio.php
@@ -1,13 +1,19 @@
 <form method="get" action="gpio.php">
     <input type="submit" value="ON" name="on">
     <input type="submit" value="OFF" name="off">
+    <br>
+    <p>LED is </p>
+    <p id="state"></p>
 </form>
 <?php
 exec("gpio mode 15 out");
-if(isset($_GET['on'])){
-    exec("gpio write 15 1");
-    echo "LED is on";}
-else if(isset($_GET['off'])){
-    exec("gpio -g write 17 0");
-    echo "LED is off";}
+while (1) {
+    if (isset($_GET['on'])) {
+        exec("gpio write 15 1");
+        echo "LED is on";
+    } else if (isset($_GET['off'])) {
+        exec("gpio -g write 17 0");
+        echo "LED is off";
+    }
+}
 ?>
