8a890cf58c83bbb20c97c34627a727a3557ae524
ehab <ehab.mohamed.nasr@gmail.com>
2016-09-10 06:09:39 +0200

    still learning

--- gpio.php
+++ gpio.php
@@ -1,9 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GPIO</title>
+</head>
+<body>
+<form method="get" action="gpio.php">
+    <input type="submit" value="ON" name="on">
+    <input type="submit" value="OFF" name="off">
+    <br>
+</form>
+</body>
+</html>
 <?php
-exec("gpio mode 15 out");
-if(isset($_GET['on'])){
-    exec("gpio write 15 1");
-    echo "LED is on";}
-else if(isset($_GET['off'])){
-    exec("gpio write 15 0");
-    echo "LED is off";}
+    exec("gpio mode 15 out");
+    if(isset($_GET['on'])){
+        exec("gpio write 15 1");
+        echo "LED is on";}
+    else if(isset($_GET['off'])){
+        exec("gpio write 15 0");
+        echo "LED is off";}
 ?>
