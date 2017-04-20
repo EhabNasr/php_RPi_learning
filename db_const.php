437449bf2baa14e3c8c77bf08d3e5a34d4cfd413
EhabNasr <ehab.mohamed.nasr@gmail.com>
2017-04-21 00:29:06 +0200

    Introducing Bootstrap..

--- db_const.php
+++ db_const.php
@@ -1,6 +1,6 @@
 <?php
 # mysql db constants DB_HOST, DB_USER, DB_PASS, DB_NAME
-const DB_HOST = '192.168.1.64';
+const DB_HOST = 'raspberrypi';
 const DB_USER = 'root';
 const DB_PASS = 'raspberry';
 const DB_NAME = 'DEVICES';
--- gpio.php
+++ gpio.php
@@ -1,21 +1,41 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>GPIO</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
 <body>
-<form method="get" action="gpio.php">
-    <input type="submit" value="ON" name="on">
-    <input type="submit" value="OFF" name="off">
-    <br>
-    <input type="submit" value="Check Lights" name="check">
-    <br>
-    <input type="submit" value="Open Door" name="openlock">
-    <input type="submit" value="Close Door" name="closelock">
-
-
-</form>
+<div class="container-fluid">
+    <form method="get" action="gpio.php">
+        <div class="form-group">
+            <div class="row">
+                <div class="col-xs-6"><input type="submit" value="ON" name="on" class="btn btn-primary btn-block"></div>
+                <div class="col-xs-6"><input type="submit" value="OFF" name="off" class="btn btn-danger btn-block"></div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-xs-12"><input type="submit" value="Check Lights" name="check" class="btn btn-info btn-block"></div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-xs-6"><input type="submit" value="Open Door" name="openlock" class="btn btn-primary btn-block"></div>
+                <div class="col-xs-6"><input type="submit" value="Close Door" name="closelock" class="btn btn-danger btn-block"></div>
+            </div>
+            <br>
+        </div>
+    </form>
+</div>
 </body>
 </html>
 <?php
@@ -23,9 +43,6 @@
     $gpio_status = exec("gpio read 14");
     if(isset($_GET['on'])){
         exec("gpio write 14 0"); //Negative Logic
-    }
-    if(isset($_GET['off'])){
-        exec("gpio write 14 0");
     }
     if(isset($_GET['off'])){
         exec("gpio write 14 1"); //Negative Logic
